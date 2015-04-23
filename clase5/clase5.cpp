64a50bd3ec6a1d0aae65201d504b2657d1f2300a
Bruno Merlo Schurmann <chulmi@bob>
2015-04-23 19:16:11 -0300

    Cosas de la clase 5

---
+++ clase5/clase5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <time.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <signal.h>
+
+using std::cout;
+using std::endl;
+using std::rand;
+using std::srand;
