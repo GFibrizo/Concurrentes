272a82eb2dd4999442c55ece9b1e6aa0981ed285
Xero-Hige <Gaston.Martinez.90@gmail.com>
2015-04-23 21:03:10 -0300

    Agrego lo que intente hacer en la clase

--- clase5/clase5.cpp
+++ clase5/clase5.cpp
@@ -15,9 +15,29 @@
 #include <signal.h>
 #include <fcntl.h>
 
+typedef struct flock s_lock;
 
 using std::cout;
 using std::endl;
 using std::rand;
 using std::srand;
 
+int main (int argc,char** argv){
+	s_lock lock;
+	lock.l_type = F_RDLCK;
+	lock.l_whence = SEEK_END;
+		
+	int fd = open(argv[1],'r');
+	fcntl(fd,F_GETLK,&lock);
+
+	if (F_UNLCK == lock.l_type){
+		cout << lock.l_pid << endl;
+	}else{
+		fcntl(fd,F_SETLK,&lock);
+		sleep(100);
+		lock.l_type = F_UNLCK;
+		fcntl(fd,F_SETLK,&lock);
+	}
+
+	close(fd);
+}
