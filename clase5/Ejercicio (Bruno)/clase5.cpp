902afa00e9f3490fdafd32bd10574cf9610e117d
Bruno Merlo Schurmann <chulmi@bob>
2015-04-27 22:23:26 -0300

    Solución en "Ejercicio (Bruno)" andando.

--- clase5/Ejercicio (Bruno)/clase5.cpp
+++ clase5/Ejercicio (Bruno)/clase5.cpp
@@ -21,37 +21,57 @@ using std::endl;
 int main(int argc, char** argv) {
     struct flock lock;
     
-    int fd = open(argv[1], O_RDONLY);
+    int fd;
     
     int pid = fork();
     if (pid == 0) {
         // Hijo
-        lock.l_type = F_RDLCK;
+        cout << "Hijo: pid " << getpid() << endl;
+        fd = open(argv[1], O_RDWR);
+        char type = argv[2][0];
+        if (type == '1') {
+            lock.l_type = F_RDLCK;
+        } else if (type == '2') {
+            lock.l_type = F_WRLCK;
+        } else {
+            lock.l_type = F_UNLCK;
+        }
         lock.l_whence = SEEK_SET;
         lock.l_start = 0;
         lock.l_len = 0;
-        cout << "Hijo: Lockeando archivo." << endl;
+        if (lock.l_type != F_UNLCK) {
+            cout << "Hijo: Lockeando archivo." << endl;
+        }
         fcntl(fd, F_SETLK, &lock);
-        sleep(10);
+        sleep(5);
         lock.l_type = F_UNLCK;
         cout << "Hijo: Desbloqueando archivo." << endl;
         fcntl(fd, F_SETLK, &lock);
+        close(fd);
     } else {
         // Padre
-        sleep(5);
-        lock.l_type = F_UNLCK;
+        sleep(2);
+        fd = open(argv[1], O_RDONLY);
+        lock.l_type = F_WRLCK;
         cout << "Padre: Testeando archivo." << endl;
         fcntl(fd, F_GETLK, &lock);
         if (lock.l_type == F_UNLCK) {
             cout << "No hay un lock en el archivo." << endl;
         } else {
             cout << "Hay un lock en el archivo." << endl;
+            cout << "Tipo: ";
+            if (lock.l_type == F_RDLCK) {
+                cout << "Lectura";
+            } else {
+                cout << "Escritura";
+            }
+            cout << endl;
+            cout << "Proceso: " << lock.l_pid << endl;
         }
+        close(fd);
     }
     
     wait(0);
-    
-    close(fd);
     
     return 0;
 }
