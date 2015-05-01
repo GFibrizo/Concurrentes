5239488c236d67f13b20b241cfb192a82cc5fc6c
Xero-Hige <gaston.martinez.90@gmail.com>
2015-05-01 16:28:37 -0300

    Refactor

--- Tp1/sources/concu_zappi.cpp
+++ Tp1/sources/concu_zappi.cpp
@@ -19,9 +19,9 @@
 
 #include <fstream>
 #include <iostream>
-#include <string>
-#include <sstream>
 #include <map>
+#include <sstream>
+#include <string>
 
 #include "logger.h"
 
@@ -33,34 +33,21 @@ using std::ifstream;
 using std::stringstream;
 using std::map;
 
-int main(int argc, char** argv) {
-
-	Logger::open_logger("run_log.log"); //TODO: Agregar opccion para sobreescribir
-
-	Logger::log(__FILE__, Logger::INFO, "Inicio configuracion");
-	map<string, int> config;
-
-	config["Recepcionistas"]=0;
-	config["Cocineras"]=0;
-	config["Cadetas"]=0;
-	config["Hornos"]=0;
-
+/*Funcion para cargar la configuracion del programa en un map*/
+void load_configurations(map<string, int>& config) {
 	ifstream file;
 	file.open("config.cfg", std::fstream::in);
-
 #ifdef __DEBUG__
 	Logger::log(__FILE__, Logger::INFO, "Configuracion inicial");
 #endif
+
 	while (file.good()) {
 		int amount = 0;
 		string concept;
-
 		file >> amount >> concept;
-
-		if (config.count(concept)==0){
+		if (config.count(concept) == 0) {
 			continue;
 		}
-
 		config[concept] = amount;
 
 #ifdef __DEBUG__
@@ -69,20 +56,50 @@ int main(int argc, char** argv) {
 		Logger::log(__FILE__, Logger::INFO, output.str());
 #endif
 	}
+}
 
-	for (std::map<string,int>::iterator it=config.begin(); it!=config.end(); ++it){
-		if (it->second <= 0){
+void initialize_configurations(map<string, int>& config) {
+	config["Recepcionistas"] = 0;
+	config["Cocineras"] = 0;
+	config["Cadetas"] = 0;
+	config["Hornos"] = 0;
+}
+
+/*Funcion que devuelve si todos los campos fueron correctamente configurados*/
+bool is_full_configured(map<string, int> config) {
+	for (std::map<string, int>::iterator it = config.begin();
+			it != config.end(); ++it) {
+		if (it->second <= 0) {
 			stringstream output;
-			output << it->first << " no tiene un valor asignado o es menor o igual a 0 ";
+			output << it->first
+					<< " no tiene un valor asignado o es menor o igual a 0 ";
 			Logger::log(__FILE__, Logger::ERROR, output.str());
-			Logger::log(__FILE__,Logger::ERROR,"Configuracion fallida");
-			Logger::log(__FILE__, Logger::INFO, "Cierre forzado");
-			return 1;
+			Logger::log(__FILE__, Logger::ERROR, "Configuracion fallida");
+			return false;
 		}
 	}
+	return true;
+}
 
+int main(int argc, char** argv) {
+
+	Logger::open_logger("run_log.log"); //TODO: Agregar opccion para sobreescribir
+
+	Logger::log(__FILE__, Logger::INFO, "Inicio configuracion");
+	map<string, int> config;
+
+	initialize_configurations(config);
+	load_configurations(config);
+
+	if (not is_full_configured(config)) {
+		Logger::log(__FILE__, Logger::ERROR, "Configuracion fallida");
+		Logger::close_logger();
+		return 1;
+	}
 	Logger::log(__FILE__, Logger::INFO, "Configuracion exitosa");
 
 	Logger::log(__FILE__, Logger::INFO, "Inicia recepcion de pedidos");
 	Logger::log(__FILE__, Logger::INFO, "Cerrada recepcion de pedidos");
+	Logger::close_logger();
+	return 0;
 }
