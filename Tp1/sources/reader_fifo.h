88c202c59f2cf938b3a5cd47e9e77670affb222c
Bruno Merlo Schurmann <chulmi@bob>
2015-05-05 20:40:55 -0300

    Fifos base, de lectura y de escritura.

---
+++ Tp1/sources/reader_fifo.h
@@ -0,0 +1,15 @@
+#ifndef READERFIFO_H_
+#define READERFIFO_H_
+
+#include "fifo.h"
+
+class ReaderFifo : public Fifo {
+public:
+	ReaderFifo(const std::string name);
+	~ReaderFifo();
+
+	void open_fifo();
+	ssize_t read_fifo(void* buffer, const ssize_t buffsize) const;
+};
+
+#endif /* READERFIFO_H_ */
