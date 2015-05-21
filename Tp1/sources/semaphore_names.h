d49a8b6ca088a37c044cec9084eb917bb4002c14
Xero-Hige <gaston.martinez.90@gmail.com>
2015-05-21 02:44:47 -0300

    Agrego los nombres de los semaforos -.-

---
+++ Tp1/sources/semaphore_names.h
@@ -0,0 +1,11 @@
+#ifndef CONCU_ZAPPI_SEMNAMES_H
+#define CONCU_ZAPPI_SEMNAMES_H
+
+#define RECEPCIONIST_SEM "call_center.cpp"
+#define CHEFS_SEM "kitchen.cpp"
+#define REQUEST_SEM "kitchen.h"
+#define CADETS_SEM "delivery.cpp"
+#define FREE_OVENS_SEM "semaphore.h"
+#define OCCUPIED_OVENS_SEM "semaphore.cpp"
+
+#endif //CONCU_ZAPPI_SEMNAMES_H
