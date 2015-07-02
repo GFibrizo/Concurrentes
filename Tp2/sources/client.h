9a8d57a034ab7d22e923eb1ec404cc84fc5611ef
chulmi <bruno290@gmail.com>
2015-07-02 16:33:15 -0300

    Licencias corregidas y/o agregadas.

--- Tp2/sources/client.h
+++ Tp2/sources/client.h
@@ -1,7 +1,7 @@
 //
-// client
-//
-// Copyright 2015 Bruno Merlo Schurmann bruno290@gmail.com 
+// Copyright 2015
+// Bruno Merlo Schurmann bruno290@gmail.com
+// Gaston Martinez gaston.martinez.90@gmail.com
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
